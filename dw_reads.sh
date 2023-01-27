c69662e22cdf964df34e8c8201dc6a8b402b8f64
Cassidy Johnson <cjohnson02@login04.hpc.rockefeller.internal>
2023-01-27 17:23:01 -0500

    Script to download raw reads.

---
+++ dw_reads.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##This document holds scripts to the download the raw reads of the assembled species differentiated by 10x, illumina, or pacbio_hifi reads
+
+LINE=($(sed -n  ${SLURM_ARRAY_TASK_ID}p ${1}))
+ 
+echo $LINE
+NAME=${LINE[0]}
+ID=${LINE[1]}
+
+cd ${ID}
+mkdir -p ./genomic_data/10x ./genomic_data/illumina ./genomic_data/pacbio_hifi
+
+#10x:
+echo "/
+sbatch --partition=vgl aws s3 cp --no-sign-request --recursive --exclude '*' --include *R1*.fastq.gz --include *R2*.fastq.gz s3://genomeark/species/${NAME}/${ID}/genomic_data/10x/ ${ID} | awk '{print $4}' > job.id"
+sbatch --partition=vgl aws s3 cp --no-sign-request --recursive --exclude '*' --include "*R1*.fastq.gz" --include "*R2*.fastq.gz" s3://genomeark/species/${NAME}/${ID}/genomic_data/10x/ ${ID} | awk '{print $4}' > job.id
+
+#illumina:
+echo "/
+sbatch --partition=vgl aws s3 cp --no-sign-request --recursive --exclude '*' --include *R1.fastq.gz --include *R2.fastq.gz s3://genomeark/species/${NAME}/${ID}/genomic_data/illumina/ ${ID} | awk '{print $4}' > job.id"
+sbatch --partition=vgl aws s3 cp --no-sign-request --recursive --exclude '*' --include "*R1.fastq.gz" --include "*R2.fastq.gz" s3://genomeark/species/${NAME}/${ID}/genomic_data/illumina/ ${ID} | awk '{print $4}' > job.id
+
+#pacbio_hifi:
+echo "/
+sbatch --partition=vgl aws s3 cp --no-sign-request --recursive --exclude '*' --include *.fastq.gz s3://genomeark/species/${NAME}/${ID}/genomic_data/pacbio_hifi/ ${ID} | awk '{print $4}' > job.id"
+sbatch --partition=vgl aws s3 cp --no-sign-request --recursive --exclude '*' --include "*.fastq.gz" s3://genomeark/species/${NAME}/${ID}/genomic_data/pacbio_hifi/ ${ID} | awk '{print $4}' > job.id
